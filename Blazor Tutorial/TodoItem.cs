5c61eaf1e606cb5dc84c8f83ab1431597eaf45d8
NAT_Games <nick.harris90@gmail.com>
2021-11-19 15:29:54 -0500

    uploaded website

---
+++ Blazor Tutorial/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class TodoItem
+{	
+		[Required]
+		[StringLength(10, ErrorMessage = "Name is too Long")]
+		public string Title { get; set; }
+		public bool IsDone { get; set; }
+	
+}
